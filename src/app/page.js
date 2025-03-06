3c8de532148f78bb3202fb6165fa3cc12532a359
Muhammadrasul21 <theboy6878@gmail.com>
2025-03-07 04:08:15 +0500

    fixed product

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const getData = async () => {
   const res = await fetch("https://dummyjson.com/products");
@@ -6,7 +6,7 @@ const getData = async () => {
   return payload;
 };
 
-const Recipe = async () => {
+const Product = async () => {
   const payload = await getData();
 
   return (
@@ -14,20 +14,29 @@ const Recipe = async () => {
       {payload?.products?.map((product) => (
         <div
           key={product.id}
-          className="p-4 flex flex-col items-center font-semibold text-lg shadow-md relative max-w-xs overflow-hidden bg-cover bg-no-repeat hover:shadow-2xl hover:transition duration-300 ease-out"
+          className="p-5 flex flex-col items-center font-semibold text-lg shadow-lg border 
+          bg-[#000] rounded-lg hover:shadow-xl transition duration-300 ease-in-out border-gray-900"
         >
-          <Image
-            src={product.thumbnail}
-            alt={product.title}
-            width={320}
-            height={250}
-            className="max-w-xs transition duration-300 ease-in-out hover:scale-110"
-          />
-          <h3 className="text-xl font-medium line-clamp-1">{product.title}</h3>
+          <div className="overflow-hidden rounded-lg">
+            <Image
+              src={product.thumbnail}
+              alt={product.title}
+              width={320}
+              height={250}
+              className="rounded-lg object-cover hover:scale-105 transition duration-300 ease-in-out cursor-pointer"
+            />
+          </div>
+
+          <h3 className="text-xl font-medium mt-3 text-white line-clamp-1 cursor-pointer">
+            {product.title}
+          </h3>
+          <p className="font-semibold text-cyan-400 px-3 py-1 rounded-lg mt-2 cursor-pointer">
+            ${product.price}
+          </p>
         </div>
       ))}
     </div>
   );
 };
 
-export default Recipe;
+export default Product;
